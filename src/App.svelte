cc9389dea4414ad7548f8c8f2f992aa63c96f805
danielasion <daniela.sion.c@gmail.com>
2024-11-04 21:45:03 -0800

    finishing up

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Button from './Button.svelte';
+</script>
+
+<div class="p-[96px] bg-gray-100">
+  
+  <div class="space-y-4">
+    <Button 
+      buttonLabel="Button 1" 
+      backgroundColor="#6B4EFF" 
+      hoverColor="#E7E7FF" 
+      textColor="#ffffff" 
+      hoverTextColor="#6B4EFF" 
+    /> 
+    
+    <Button 
+      buttonLabel="Button 2" 
+      backgroundColor="#5538EE" 
+      hoverColor="#C6C4FF" 
+      textColor="#ffffff" 
+      hoverTextColor="#5538EE" 
+    /> 
+    
+    <Button 
+      buttonLabel="Button 3" 
+      backgroundColor="#E3E5E5" 
+      hoverColor="#E3E5E5" 
+      textColor="#979C9E" 
+      hoverTextColor="#979C9E" 
+    /> 
+  </div>
+</div>
+
+<style>
+  h1 {
+    font-family: 'Inter', sans-serif;
+    color: #333; 
+  }
+</style>
+
+<svelte:head>
+  <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet" />
+</svelte:head>
---
+++ src/Button.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let buttonLabel = "Button"; 
+  export let backgroundColor = ''; 
+  export let hoverColor = ''; 
+  export let textColor = '#ffffff'; 
+  export let hoverTextColor = '#ffffff'; 
+
+  let isHovered = false;
+</script>
+
+<button
+  class="w-[327px] h-[327px] rounded-full transition duration-300 ease-in-out"
+  style={`background-color: ${isHovered ? hoverColor : backgroundColor}; color: ${isHovered ? hoverTextColor : textColor}; `}
+  on:mouseenter={() => isHovered = true}
+  on:mouseleave={() => isHovered = false}
+>
+  {buttonLabel}
+</button>
+
+<style>
+  button {
+    font-family: 'Inter', sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 1; 
+    text-align: center;
+    border: none; 
+    cursor: pointer; 
+    display: inline-flex; 
+    justify-content: center; 
+    align-items: center; 
+		width: 327px;
+		height: 48px;
+  }
+</style>
